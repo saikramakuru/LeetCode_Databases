12bcbf7162d0dcdffc211e316ebddbccbf0b1b93
Sai K Ramakuru <ramakuru@tamu.edu>
2016-04-10 17:58:45 -0500

    Update 197_Rising Temperatures.sql

--- 197_Rising Temperatures.sql
+++ 197_Rising Temperatures.sql
@@ -1,13 +1,24 @@
-Use Practice
+/* SQL SERVER FUNTION OF DATEPART IS USED */
+
+/* Create table Weather */
 
 Create table Weather(
 Id int PRIMARY KEY IDENTITY(1,1),
 Date Date,
 Temperature Float)
 
-Select * from Weather
+/*197. RISING TEMPERATURES SQL QUERY */
+
 
 Select W1.Id 
 From Weather W1, Weather W2 
 Where DATEPART(dd, W1.Date) = ( DATEPART (dd, W2.Date) +1)
 AND W1.Temperature> W2.Temperature
+
+/* If you are using MySQL, use TO_Days Funtion */
+
+select w2.Id
+from Weather w1, Weather w2 
+Where
+w1.Temperature < w2.Temperature
+AND TO_DAYS(w2.Date)-TO_DAYS(w1.Date) = 1;
